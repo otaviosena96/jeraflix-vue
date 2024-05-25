38c60c13f39ea8714eb3dfef8717bc9e960777a8
Otávio Sena <otaviosena.dev@proton.me>
2024-05-25 15:44:48 -0400

    profile crud start

--- src/views/CreateProfile.vue
+++ src/views/CreateProfile.vue
@@ -4,50 +4,37 @@
   <div class="container mt-5">
     <h2 class="text-primary text-center mb-4">ESCOLHA UM PERFIL</h2>
     <div class="row">
-      <div class="col-md-4 mb-4" v-for="(profile, index) in profiles" :key="index">
-        <el-card :style="{ backgroundColor: profile.color, minHeight: '10rem', minWidth: '12rem', display: 'flex', alignItems: 'center', justifyContent: 'center' }">          
-            <span class="text-center">{{ profile.name }}</span>        
+      <div class="col-md-4 mb-4 me-5" v-for="(profile, index) in profiles" :key="index">
+        <el-card class="d-flex align-items-center justify-content-center" :style="{ backgroundColor: profile.color, minHeight: '10rem', minWidth: '12rem', marginRight: '10px' }">
+          <span class="text-center align-item-center">{{ profile.name }}</span>
         </el-card>
       </div>
       <div class="col-md-4 mb-4">
-        <el-card @click="addProfile" :style="{ backgroundColor:'#007bff', minHeight: '10rem', minWidth: '12rem', display: 'flex', alignItems: 'center', justifyContent: 'center', cursor: 'pointer' }">
-              <font-awesome-icon icon="plus" />
+        <el-card @click="showModal" class="d-flex align-items-center justify-content-center" :style="{ backgroundColor:'#007bff', minHeight: '10rem', minWidth: '12rem', cursor: 'pointer' }">
+          <font-awesome-icon icon="plus" />
         </el-card>
       </div>
     </div>
   </div>
+  <CreateProfileModal v-model:visible="isModalVisible"></CreateProfileModal>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+import CreateProfileModal from './modals/CreateProfileModal.vue';
+
+const isModalVisible = ref(false);
 
 const profiles = ref([{
   name: 'Perfil 1',
-  color: '#007bff'
+  color: '#007ff'
 }]);
 
-const addProfile = () => {
-  // Lógica para adicionar um novo perfil
-  console.log('Add new profile');
-};
+const showModal = () => {
+  isModalVisible.value = true;
+}
 
 onMounted(() => {
   console.log('mounted');
 });
 </script>
-
-<style scoped>
-.add-profile {
-  font-size: 3em; /* Tornar o ícone maior */
-  color: #fff;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%; /* Centralizar verticalmente */
-}
-.container {
-  max-width: 800px;
-  margin: 0 auto;
-}
-</style>
---
+++ src/views/modals/CreateProfileModal.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-if="visible" class="modal d-block" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">CRIE UM PERFIL</h5>
+          <button type="button" class="btn-close" aria-label="Close" @click="closeModal"></button>
+        </div>
+        <div class="modal-body">
+          <el-form>
+            <el-form-item label="Nome do Perfil">
+              <el-input v-model="form.name" placeholder="Digite o nome"></el-input>
+            </el-form-item>
+            <el-form-item label="Cor do Perfil">
+              <el-color-picker v-model="form.color" />
+            </el-form-item>
+          </el-form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" @click="createProfile">
+            
+            <span>SALVAR</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, reactive } from 'vue';
+
+const form = reactive({
+  name: '',
+  color: '',
+});
+
+const props = defineProps({
+  visible: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+const emits = defineEmits(['update:visible']);
+
+const createProfile = () => {
+  console.log(form);
+  emits('update:visible', false);
+}
+
+const closeModal = () => {
+  emits('update:visible', false);
+}
+</script>
